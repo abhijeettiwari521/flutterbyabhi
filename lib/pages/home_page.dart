e0092e4820b70209cb1718d80e337d5297f62750
Abhijeettiwari521 <abhijeettiwari521@gmail.com>
2022-04-29 20:36:19 +0530

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_2/models/catalog.dart';
 import 'package:flutter_application_2/widgets/drawer.dart';
 import 'package:flutter_application_2/widgets/item_widget.dart';
 
 //*day11 completed
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 1;
   final String name = "Abhi";
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dumyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App", style: TextStyle(color: Colors.black)),
+        title: Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
