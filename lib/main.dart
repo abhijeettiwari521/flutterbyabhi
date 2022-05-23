8ebb10caeb957762da8c8daaaf4ebc72671584a5
Abhijeettiwari521 <abhijeettiwari521@gmail.com>
2022-05-23 22:02:35 +0530

    day26 Vxstate and Vxstore completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/core/store.dart';
 import 'package:flutter_application_2/pages/cart_page.dart';
 import 'package:flutter_application_2/pages/home_page.dart';
 import 'package:flutter_application_2/pages/login_page.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_application_2/utils/routes.dart';
 import 'package:flutter_application_2/widgets/themes.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(VxState(
+    store: MyStore(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
