c8565b80f0d1af0a6bc362e80cc013fceffaa658
Abhijeettiwari521 <abhijeettiwari521@gmail.com>
2022-05-25 21:51:13 +0530

    day27 VxMutation completed

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/core/store.dart';
 import 'package:flutter_application_2/models/cart.dart';
 import 'package:flutter_application_2/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
   const AddToCart({
     Key? key,
@@ -12,36 +13,25 @@ class AddToCart extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AddToCart> createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
-  final _cart = CartModel();
-  @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog);
+    VxState.watch(context, on: [AddMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+    bool isInCart = _cart.items.contains(catalog);
     return ElevatedButton(
-        onPressed: () {
-          isInCart = isInCart.toggle();
-          if (isInCart) {
-            final _catalog = CatalogModel();
-
-            _cart.catalog = _catalog;
-            _cart.add(widget.catalog);
-            setState(() {});
-          }
-        },
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(
-            // ignore: deprecated_member_use
-            context.theme.buttonColor,
-          ),
+      onPressed: () {
+        if (!isInCart) {
+          AddMutation(catalog);
+        }
+      },
+      style: ButtonStyle(
+          // ignore: deprecated_member_use
+          backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
           shape: MaterialStateProperty.all(
             const StadiumBorder(),
-          ),
-        ),
-        child: isInCart
-            ? const Icon(Icons.done)
-            : const Icon(CupertinoIcons.cart_badge_plus));
+          )),
+      child: isInCart
+          ? const Icon(Icons.done)
+          : const Icon(CupertinoIcons.cart_badge_plus),
+    );
   }
 }
