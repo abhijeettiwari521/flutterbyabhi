a305b17f34784ef3149e11b2218f93bfde0b3e20
Abhijeettiwari521 <abhijeettiwari521@gmail.com>
2022-05-28 22:09:15 +0530

    day29   badge and http completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_2/core/store.dart';
+import 'package:flutter_application_2/models/cart.dart';
 import 'dart:convert';
 import 'package:flutter_application_2/models/catalog.dart';
 import 'package:flutter_application_2/utils/routes.dart';
 import 'package:flutter_application_2/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_application_2/widgets/home_widgets/catalog_list.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 //*day11 completed
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final int days = 1;
-  final String name = "Abhi";
+  final int days = 30;
+
+  final String name = "Codepur";
+
+  final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
+
   @override
   void initState() {
     super.initState();
@@ -29,8 +36,11 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(const Duration(seconds: 3));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
-    final decodeData = jsonDecode(catalogJson);
-    var productsData = decodeData["products"];
+
+    //final response = await http.get(Uri.parse(url));
+    // final catalogJson = response.body;
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
         .map<Item>((item) => Item.fromMap(item))
         .toList();
@@ -39,13 +49,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
         backgroundColor: context.canvasColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-          // ignore: deprecated_member_use
-          backgroundColor: context.theme.buttonColor,
-          child: const Icon(CupertinoIcons.cart, color: Colors.white),
+        floatingActionButton: VxBuilder(
+          mutations: const {AddMutation, RemoveMutation},
+          builder: (context, _, __) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            // ignore: deprecated_member_use
+            backgroundColor: context.theme.buttonColor,
+            child: const Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(
+              color: Vx.gray200,
+              size: 22,
+              count: _cart.items.length,
+              textStyle: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )),
         ),
         body: SafeArea(
           child: Container(
@@ -54,10 +78,8 @@ class _HomePageState extends State<HomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const CatalogHeader(),
-                // ignore: unnecessary_null_comparison
-                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-                  // ignore: prefer_const_constructors
-                  CatalogList().py16().expand()
+                if (CatalogModel.items.isNotEmpty)
+                  const CatalogList().py16().expand()
                 else
                   const CircularProgressIndicator().centered().expand(),
               ],
